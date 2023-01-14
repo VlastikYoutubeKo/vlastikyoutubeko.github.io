604247cdd443753c849ea2351cdb5e638f9220ea
VlastikYoutubeKo <56507931+VlastikYoutubeKo@users.noreply.github.com>
2023-01-14 14:45:33 +0000

    Oprava cislo 53

--- test/script.js
+++ test/script.js
@@ -24,24 +24,4 @@ links.forEach((link) => {
         // display the number of clicks for the link
         console.log(`Link ${link.href} has been clicked ${clicks[link.href]} times`);
     });
-
-
-    // add a mouseover event listener to the link
-    link.addEventListener('mouseover', () => {
-        // generate a random color
-        const randomColor = getRandomColor();
-        // change the color of the link with a smooth transition
-        link.style.transition = "color 0.5s ease-in-out";
-        link.style.color = randomColor;
-    });
-});
-
-// function to generate a random color
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+})
