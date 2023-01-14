6b2946ee4a8f4931afa46f9cbbbe7712a3c68074
VlastikYoutubeKo <56507931+VlastikYoutubeKo@users.noreply.github.com>
2023-01-14 13:23:58 +0000

    a

--- test/script.js
+++ test/script.js
@@ -1,21 +1,17 @@
 // get all the links on the page
 const links = document.querySelectorAll('a');
 
-// create an object to store the number of clicks for each link
-const clicks = {};
-
 // loop through each link
 links.forEach((link) => {
-    // set the initial number of clicks for the link to 0
-    clicks[link.href] = 0;
-
     // add a click event listener to the link
     link.addEventListener('click', () => {
-        // increment the number of clicks for the link
-        clicks[link.href]++;
-        // display the number of clicks for the link
-        console.log(`Link ${link.href} has been clicked ${clicks[link.href]} times`);
+        // send the event to Google Analytics
+        gtag('event', 'click', {
+            'event_category': 'outbound',
+            'event_label': link.href
+        });
     });
+
 
     // add a mouseover event listener to the link
     link.addEventListener('mouseover', () => {
